eed37cf72a22ef154a613330af55d307573f38fd
Joshua Castaneda <j6castaneda@ucsd.edu>
2025-05-27 22:39:12 -0700

    Used wrong pattern?

--- sw.js
+++ sw.js
@@ -41,16 +41,26 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. DONE - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-
   event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    // B8. Done - If the request is in the cache, return with the cached version.
+    // B8. DONE - If the request is in the cache, return with the cached version.
     //            Otherwise fetch the resource, add it to the cache, and return
     //            network response.
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+    return cache.match(event.request.url).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request).then((fetchedResponse) => {
         cache.put(event.request, fetchedResponse.clone());
+        // Return the network response
         return fetchedResponse;
       });
-    })
+    });
   }));
+
+    // return cache.match(event.request).then((cachedResponse) => {
+    //   return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+    //     cache.put(event.request, fetchedResponse.clone());
+    //     return fetchedResponse;
+    //   });
+    // });
+  // }));
 });
