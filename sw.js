183dc0c4575e9ec20a4296f007e2179525c7a1d4
Joshua Castaneda <j6castaneda@ucsd.edu>
2025-05-27 21:36:50 -0700

    Expose B7

--- sw.js
+++ sw.js
@@ -39,9 +39,13 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
+  // B7. DONE - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.
+
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    // B8. TODO - If the request is in the cache, return with the cached version.
+    //            Otherwise fetch the resource, add it to the cache, and return
+    //            network response.
+  }));
 });
